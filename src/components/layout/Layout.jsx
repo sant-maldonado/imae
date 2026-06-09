1d4f6e48e79e38bc9b8b77e23f21af48af5493e3
sant-maldonado <sm.santiagomaldonado@gmail.com>
2026-06-09 15:39:46 -0300

    feat: add page transition animation
    - Fade-in + translateY animation on route change
    - Uses location.pathname as key to trigger re-animation

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,9 +1,10 @@
 import { useState, useEffect } from 'react'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 import Sidebar from './Sidebar'
 import Header from './Header'
 
 export default function Layout() {
+  const location = useLocation()
   const [sidebarOpen, setSidebarOpen] = useState(false)
 
   useEffect(() => {
@@ -28,7 +29,9 @@ export default function Layout() {
       <div className="flex-1 flex flex-col min-w-0 overflow-hidden">
         <Header onToggleSidebar={() => setSidebarOpen((v) => !v)} />
         <main className="flex-1 overflow-auto p-3 md:p-6">
-          <Outlet />
+          <div key={location.pathname} className="page-enter">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
